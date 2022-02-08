533424a6bc23f77ab44941b1f7310ba381145ba1
- <d.b_official@mail.ru>
2022-02-09 02:27:34 +0300

    added demo interface

---
+++ NumericalMethods.WPFApplication/InterpolationNode.cs
@@ -0,0 +1,3 @@
+﻿using NumericalMethods.Approximation.Interpolations.Interfaces;
+namespace NumericalMethods.WPFApplication;
+public record class InterpolationNode(double X, double Y) : IInterpolationNode;
--- NumericalMethods.WPFApplication/MainWindow.xaml.cs
+++ NumericalMethods.WPFApplication/MainWindow.xaml.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿using NumericalMethods.Approximation.Interpolations;
+using NumericalMethods.Approximation.Interpolations.Interfaces;
+
+using org.mariuszgromada.math.mxparser;
+
+using ScottPlot;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +27,53 @@ namespace NumericalMethods.WPFApplication
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<InterpolationNode> nodes;
+        private const double StartX = -4 * Math.PI;
+        private const double EndX = 4 * Math.PI;
+        private const double Step = Math.PI / 2;
         public MainWindow()
         {
             InitializeComponent();
+            Function function = new Function("f(x) = sin(x)");
+            nodes = new List<InterpolationNode>();
+
+            for (double x = StartX; x <= EndX; x += Step)
+            {
+                nodes.Add(new InterpolationNode(x, function.calculate(x)));
+            }
+
+            foreach (var item in Enum.GetValues(typeof(InterpolationFunctionType)))
+            {
+                Selector.Items.Add(item.ToString());
+            }
+
+            Width = 1300;
+            Height = 750;
+        }
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainChart.Plot.Clear();
+            MainChart.Refresh();
+            IInterpolationFunction? interpolation_function = InterpolationBuilder.Build(nodes, (InterpolationFunctionType)Enum.Parse(typeof(InterpolationFunctionType), Selector.SelectedValue.ToString()));
+            if (interpolation_function is null) return;
+            var interpolation_xs = new List<double>();
+            var interpolation_ys = new List<double>();
+            for (double x = StartX; x <= EndX; x += 0.1)
+            {
+                var y = interpolation_function.Calculate(x);
+                if (y is null) continue;
+
+                interpolation_xs.Add(x);
+                interpolation_ys.Add((double)y);
+            }
+
+            MainChart.Plot.AddScatter(interpolation_xs.ToArray(), interpolation_ys.ToArray(), lineWidth: 3, markerSize: 4, label: "interpolation");
+            var xs = nodes.Select(node => node.X).ToArray();
+            var ys = nodes.Select(node => node.Y).ToArray();
+            MainChart.Plot.AddScatter(xs, ys, lineWidth: 0, markerSize: 5, label: "data");
+            MainChart.Plot.Legend();
+            MainChart.Refresh();
         }
     }
 }
