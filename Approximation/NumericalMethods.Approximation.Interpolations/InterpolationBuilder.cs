529dca7624b1b86674c1b53846da0ac3a5fbf533
- <d.b_official@mail.ru>
2022-02-08 19:42:27 +0300

    added definition of Buil method in InterpolationBuilder class

--- Approximation/NumericalMethods.Approximation.Interpolations/InterpolationBuilder.cs
+++ Approximation/NumericalMethods.Approximation.Interpolations/InterpolationBuilder.cs
@@ -1,10 +1,15 @@
 ﻿using NumericalMethods.Approximation.Interpolations.Interfaces;
+using NumericalMethods.Approximation.Interpolations.InterpolationFuncitons.Linear;
 
 namespace NumericalMethods.Approximation.Interpolations;
 public static class InterpolationBuilder
 {
     public static IInterpolationFunction Build(IEnumerable<IInterpolationNode> interpolation_nodes, InterpolationFunctionType function_type)
     {
-
+        return function_type switch
+        {
+            InterpolationFunctionType.Linear => new LinearInterpolationFunction(interpolation_nodes),
+            _ => null
+        };
     }
 }
