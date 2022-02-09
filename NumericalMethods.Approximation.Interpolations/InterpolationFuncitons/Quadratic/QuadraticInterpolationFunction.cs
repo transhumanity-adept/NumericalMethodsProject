03b6ca53d01cba35182aff5259378cfb363a5c64
Данила Савенков <savenkov-lokin2013@yandex.ru>
2022-02-09 21:35:37 +0300

    Сделаны классы, представляющие квадратичную интерполяцию

--- NumericalMethods.Approximation.Interpolations/InterpolationFuncitons/Quadratic/QuadraticInterpolationFunction.cs
+++ NumericalMethods.Approximation.Interpolations/InterpolationFuncitons/Quadratic/QuadraticInterpolationFunction.cs
@@ -4,66 +4,64 @@ namespace NumericalMethods.Approximation.Interpolations.InterpolationFuncitons.Q
     internal class QuadraticInterpolationFunction:IInterpolationFunction
     {
         private readonly IEnumerable<IInterpolationNode> _interpolation_nodes;
-        public QuadraticInterpolationFunction()
+        private readonly List<(QuadraticFunction quadraticFunction, double startArgument)> _functions_of_interpolation_intervals;
+        public QuadraticInterpolationFunction(IEnumerable<IInterpolationNode> _interpolation_nodes)
         {
-            
+            this._interpolation_nodes = _interpolation_nodes;
+            this._functions_of_interpolation_intervals = CreateFunctionsOfInterpolationIntervals(); 
         }
-        private List<QuadraticFunction> CreateFunctionsOfInterpolationIntervals()
+        private List<(QuadraticFunction quadraticFunction, double startArgument)> CreateFunctionsOfInterpolationIntervals()
         {
-            List<QuadraticFunction> result = new List<QuadraticFunction>();
+            List<(QuadraticFunction quadraticFunction,double startArgument)> result = new List<(QuadraticFunction quadraticFunction, double startArgument)>();
             List<IInterpolationNode> pointsForConstructingQuadraticFunction = new List<IInterpolationNode>();
-            for (int i = 0; i < _interpolation_nodes.Count(); i++)
+            int count_nodes = _interpolation_nodes.Count();
+            for (int i = 0; i < count_nodes; i++)
             {
-                if (i + 2 >= _interpolation_nodes.Count())
+                if (i + 2 >= count_nodes)
                     break;
-                pointsForConstructingQuadraticFunction.AddRange(new List<IInterpolationNode>() { _interpolation_nodes.ElementAt(i), _interpolation_nodes.ElementAt(i+1), _interpolation_nodes.ElementAt(i+2) });
-                result.Add(MethodKrammer(CreateSystemOfEquations(pointsForConstructingQuadraticFunction)));
+                pointsForConstructingQuadraticFunction.AddRange(new List<IInterpolationNode>() { _interpolation_nodes.ElementAt(i), _interpolation_nodes.ElementAt(i + 1), _interpolation_nodes.ElementAt(i + 2) });
+                if (i == 0)
+                    result.Add((MethodKrammer(CreateSystemOfEquations(pointsForConstructingQuadraticFunction)), pointsForConstructingQuadraticFunction.First().X));
+                else
+                    result.Add((MethodKrammer(CreateSystemOfEquations(pointsForConstructingQuadraticFunction)), pointsForConstructingQuadraticFunction[1].X));
                 pointsForConstructingQuadraticFunction.Clear();
             }
-            return resu;
+            return result;
 
         }
         public double? Calculate(double argument)
         {
-            List<double> coeffsOfQuadraticEquation;
-            if (i + 2 >= Points.Count)
-                break;
-            pointsForConstructingQuadraticFunction.AddRange(new List<Point>() { Points[i], Points[i + 1], Points[i + 2] });
-            coeffsOfQuadraticEquation = MethodKrammer(CreateSystemOfEquations(pointsForConstructingQuadraticFunction));
-            if (i == 0)
-                for (double x = pointsForConstructingQuadraticFunction.First().X; x < pointsForConstructingQuadraticFunction.Last().X; x += Step)
+            QuadraticFunction? quadraticFunction = null;
+            for (int i = 0; i < _functions_of_interpolation_intervals.Count-1; i++)
+            {
+                if(argument<= _functions_of_interpolation_intervals[i+1].startArgument && argument >= _functions_of_interpolation_intervals[i].startArgument)
                 {
-                    intermediatePoints.Add(new Point(x, coeffsOfQuadraticEquation[0] * Math.Pow(x, 2) + coeffsOfQuadraticEquation[1] * x + coeffsOfQuadraticEquation[2]));
+                    quadraticFunction = _functions_of_interpolation_intervals[i].quadraticFunction;
+                    break;
                 }
-            else
-                for (double x = pointsForConstructingQuadraticFunction[1].X; x < pointsForConstructingQuadraticFunction.Last().X; x += Step)
-                {
-                    intermediatePoints.Add(new Point(x, coeffsOfQuadraticEquation[0] * Math.Pow(x, 2) + coeffsOfQuadraticEquation[1] * x + coeffsOfQuadraticEquation[2]));
-                }
-
-            return intermediatePoints;
+            }
+            return quadraticFunction is null ? null : quadraticFunction.Calculate(argument);
         }
 
-        private List<QuadraticFunction> CreateSystemOfEquations(List<IInterpolationNode> pointsForConstructingQuadraticFunction)
+        private List<(QuadraticFunction quadraticFunction,double y)> CreateSystemOfEquations(List<IInterpolationNode> pointsForConstructingQuadraticFunction)
         {
-            List<QuadraticFunction> quadraticEquations = new List<QuadraticFunction>();
+            List<(QuadraticFunction quadraticFunction, double y)> quadraticEquations = new List<(QuadraticFunction quadraticFunction, double y)> ();
             foreach (IInterpolationNode node in pointsForConstructingQuadraticFunction)
             {
-                quadraticEquations.Add(new QuadraticFunction(Math.Pow(node.X, 2.0), node.X, 1, (double)node.Y));
+                quadraticEquations.Add((new QuadraticFunction(Math.Pow(node.X, 2.0), node.X, 1),node.Y));
             }
 
             return quadraticEquations;
         }
 
-        private List<double> MethodKrammer(List<QuadraticFunction> quadraticFunction)
+        private QuadraticFunction MethodKrammer(List<(QuadraticFunction quadraticFunction, double y)> quadraticEquations)
         {
-            double determinant = FindDeterminantMatrix(FormingMatrix(quadraticFunction, 0));
-            return new List<double>()
-            {
-                FindDeterminantMatrix(FormingMatrix(quadraticFunction,1))/determinant,
-                FindDeterminantMatrix(FormingMatrix(quadraticFunction,2))/determinant,
-                FindDeterminantMatrix(FormingMatrix(quadraticFunction,3))/determinant
-            };
+            double determinant = FindDeterminantMatrix(FormingMatrix(quadraticEquations, 0));
+            return new QuadraticFunction(
+                FindDeterminantMatrix(FormingMatrix(quadraticEquations, 1)) / determinant,
+                FindDeterminantMatrix(FormingMatrix(quadraticEquations, 2)) / determinant,
+                FindDeterminantMatrix(FormingMatrix(quadraticEquations, 3)) / determinant
+            );
         }
 
         private List<List<double>> SplittingMatrix(List<List<double>> matrix, (int i, int j) indexDeleteElement)
@@ -102,40 +100,40 @@ namespace NumericalMethods.Approximation.Interpolations.InterpolationFuncitons.Q
             }
         }
 
-        private List<List<double>> FormingMatrix(List<QuadraticFunction> quadraticFunction, int indexDeterminant)
+        private List<List<double>> FormingMatrix(List<(QuadraticFunction quadraticFunction, double y)> quadraticEquations, int indexDeterminant)
         {
             List<List<double>> matrix = new List<List<double>>();
-            quadraticFunction.ForEach(quadraticEquation =>
+            quadraticEquations.ForEach(quadraticEquation =>
             {
                 matrix.Add(new List<double>());
                 switch (indexDeterminant)
                 {
                     case 0:
                         {
-                            matrix.Last().Add(quadraticEquation.A);
-                            matrix.Last().Add(quadraticEquation.B);
-                            matrix.Last().Add(quadraticEquation.C);
+                            matrix.Last().Add(quadraticEquation.quadraticFunction.A);
+                            matrix.Last().Add(quadraticEquation.quadraticFunction.B);
+                            matrix.Last().Add(quadraticEquation.quadraticFunction.C);
                             break;
                         }
                     case 1:
                         {
-                            matrix.Last().Add(quadraticEquation.Y);
-                            matrix.Last().Add(quadraticEquation.B);
-                            matrix.Last().Add(quadraticEquation.C);
+                            matrix.Last().Add(quadraticEquation.y);
+                            matrix.Last().Add(quadraticEquation.quadraticFunction.B);
+                            matrix.Last().Add(quadraticEquation.quadraticFunction.C);
                             break;
                         }
                     case 2:
                         {
-                            matrix.Last().Add(quadraticEquation.A);
-                            matrix.Last().Add(quadraticEquation.Y);
-                            matrix.Last().Add(quadraticEquation.C);
+                            matrix.Last().Add(quadraticEquation.quadraticFunction.A);
+                            matrix.Last().Add(quadraticEquation.y);
+                            matrix.Last().Add(quadraticEquation.quadraticFunction.C);
                             break;
                         }
                     case 3:
                         {
-                            matrix.Last().Add(quadraticEquation.A);
-                            matrix.Last().Add(quadraticEquation.B);
-                            matrix.Last().Add(quadraticEquation.Y);
+                            matrix.Last().Add(quadraticEquation.quadraticFunction.A);
+                            matrix.Last().Add(quadraticEquation.quadraticFunction.B);
+                            matrix.Last().Add(quadraticEquation.y);
                             break;
                         }
                 }
@@ -144,4 +142,4 @@ namespace NumericalMethods.Approximation.Interpolations.InterpolationFuncitons.Q
         }
     }
 }
-}
+
