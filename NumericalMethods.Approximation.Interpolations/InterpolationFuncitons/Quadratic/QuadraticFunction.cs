d791d462a5270d1b47c45448bf076c7fcdf470bc
Данила Савенков <savenkov-lokin2013@yandex.ru>
2022-02-09 21:48:00 +0300

    Решение проблем

--- NumericalMethods.Approximation.Interpolations/InterpolationFuncitons/Quadratic/QuadraticFunction.cs
+++ NumericalMethods.Approximation.Interpolations/InterpolationFuncitons/Quadratic/QuadraticFunction.cs
@@ -1,6 +1,6 @@
 ﻿namespace NumericalMethods.Approximation.Interpolations.InterpolationFuncitons.Quadratic
 {
-    internal record class QuadraticFunction(double A,double B, double C,double Y)
+    internal record class QuadraticFunction(double A,double B, double C)
     {
         public double Calculate(double argument) => A * Math.Pow(argument, 2) + B * argument + C;
     }
