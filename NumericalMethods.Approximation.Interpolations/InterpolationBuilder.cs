9f2f9532ed3c26d9e673d19111c608ff41aaef75
- <d.b_official@mail.ru>
2022-02-09 17:55:35 +0300

    added cubic interpolation

--- NumericalMethods.Approximation.Interpolations/InterpolationBuilder.cs
+++ NumericalMethods.Approximation.Interpolations/InterpolationBuilder.cs
@@ -1,14 +1,16 @@
 ﻿using NumericalMethods.Approximation.Interpolations.Interfaces;
+using NumericalMethods.Approximation.Interpolations.InterpolationFuncitons.Cubic;
 using NumericalMethods.Approximation.Interpolations.InterpolationFuncitons.Linear;
 
 namespace NumericalMethods.Approximation.Interpolations;
 public static class InterpolationBuilder
 {
-    public static IInterpolationFunction Build(IEnumerable<IInterpolationNode> interpolation_nodes, InterpolationFunctionType function_type)
+    public static IInterpolationFunction? Build(IEnumerable<IInterpolationNode> interpolation_nodes, InterpolationFunctionType function_type)
     {
         return function_type switch
         {
             InterpolationFunctionType.Linear => new LinearInterpolationFunction(interpolation_nodes),
+            InterpolationFunctionType.Cubic => new СubicInterpolationFunction(interpolation_nodes),
             _ => null
         };
     }
---
+++ NumericalMethods.Approximation.Interpolations/InterpolationFuncitons/Cubic/SweepMethod.cs
@@ -0,0 +1,30 @@
+﻿namespace NumericalMethods.Approximation.Interpolations.InterpolationFuncitons.Cubic;
+internal record class SweepMethod(double[] Left, double[] Center, double[] Right, double[] FreeMembers)
+{
+    public List<double> Calculate()
+    {
+        List<(double a, double b)> coefficients = new List<(double a, double b)>() { (-Right[0] / Center[0], FreeMembers[0] / Center[0]) };
+        for (int i = 1; i < FreeMembers.Length - 1; i++)
+        {
+            double e = (Left[i - 1] * coefficients[i - 1].a) + Center[i];
+            double a = -Right[i] / e;
+            double b = (FreeMembers[i] - (Left[i - 1] * coefficients[i - 1].b)) / e;
+            coefficients.Add((a, b));
+        }
+
+        List<double> result = new List<double>()
+            {
+                (FreeMembers[^1] - (Left[^1] * coefficients[^1].b)) /
+                (Center[^1] + (Left[^1] * coefficients[^1].a))
+            };
+        coefficients.Reverse();
+        for (int i = 0; i < coefficients.Count; i++)
+        {
+            double x = (coefficients[i].a * result[i]) + coefficients[i].b;
+            result.Add(x);
+        }
+
+        result.Reverse();
+        return result;
+    }
+}
