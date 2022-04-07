14722845699931196174690ca97f4aefce74a42d
Семён Поздняков <santaspolusa@outlook.com>
2022-04-07 16:12:44 +0300

    Описан класс Квадратная матрица

--- NumericalMethods.Core/SquareMatrix.cs
+++ NumericalMethods.Core/SquareMatrix.cs
@@ -11,11 +11,15 @@ public class SquareMatrix
     public SquareMatrix(int size)
     {
         _data = new double[size, size];
+        Size = size;
     }
     public SquareMatrix(double [,] matrix)
     {
         _data = matrix;
+        Size = matrix.GetLength(0);
     }
+
+    public int Size { get; set; }
 
     public double this[int row, int col]
     {
@@ -37,12 +41,12 @@ public class SquareMatrix
     {
         double max = 0;
 
-        for (int i = 0; i < data.GetLength(0); i++)
+        for (int i = 0; i < _data.GetLength(0); i++)
         {
             double row_sum = 0;
-            for (int j = 0; j < data.GetLength(1); j++)
+            for (int j = 0; j < _data.GetLength(1); j++)
             {
-                row_sum += Math.Abs(data[i,j]);
+                row_sum += Math.Abs(_data[i,j]);
             }
             if (row_sum > max) max = row_sum;
 
@@ -50,24 +54,22 @@ public class SquareMatrix
         return max;
     }
 
-    SquareMatrix GetMatrixWithout(double[,] matrix, int index_row, int index_column)
+    SquareMatrix GetMatrixWithout(int index_row, int index_column)
     {
-        //double[,] new_matrix = new double[matrix.GetLength(0) - 1, matrix.GetLength(1) - 1];
-        SquareMatrix new_matrix = new SquareMatrix(data.GetLength(0));
-
-
-        for (int i = 0; i < new_matrix.GetLength(0); i++)
+        SquareMatrix new_matrix = new SquareMatrix(_data.GetLength(0) - 1);
+        
+        for (int i = 0; i < new_matrix.Size; i++)
         {
-            for (int j = 0; j < new_matrix.GetLength(1); j++)
+            for (int j = 0; j < new_matrix.Size; j++)
             {
                 if (i >= index_row && j >= index_column)
-                    new_matrix[i, j] = matrix[i + 1, j + 1];
+                    new_matrix[i, j] = _data[i + 1, j + 1];
                 else if (i >= index_row)
-                    new_matrix[i, j] = matrix[i + 1, j];
+                    new_matrix[i, j] = _data[i + 1, j];
                 else if (j >= index_row)
-                    new_matrix[i, j] = matrix[i, j + 1];
+                    new_matrix[i, j] = _data[i, j + 1];
                 else
-                    new_matrix[i, j] = matrix[i, j];
+                    new_matrix[i, j] = _data[i, j];
             }
         }
         return new_matrix;
@@ -75,31 +77,34 @@ public class SquareMatrix
 
     double GetDeterminant()
     {
-        int size = matrix.GetLength(0);
+        int size = _data.GetLength(0);
 
         switch (size)
         {
-            case 1: return matrix[0, 0];
-            case 2: return matrix[0, 0] * matrix[1, 1] - matrix[1, 0] * matrix[0, 1];
+            case 1: return _data[0, 0];
+            case 2: return _data[0, 0] * _data[1, 1] - _data[1, 0] * _data[0, 1];
             default:
                 double determinant = 0;
                 for (int column = 0; column < size; column++)
                 {
-                    determinant += (column % 2 == 0 ? 1 : -1) * matrix[0, column] * GetDeterminant(GetMatrixWithout(matrix, 0, column));
+                    determinant += (column % 2 == 0 ? 1 : -1) * _data[0, column] *  (GetMatrixWithout(0, column).GetDeterminant());
                 }
                 return determinant;
         }
     }
 
-    double[,] GetInverse()
+    SquareMatrix GetInverse()
     {
-        double det = GetDeterminant(matrix);
-        double[,] algebraicadd = GetAlgebraicAddition(matrix);
-        double[,] trans = GetTranspose(algebraicadd);
-        double[,] result = new double[matrix.GetLength(0), matrix.GetLength(1)];
-        for (int i = 0; i < result.GetLength(0); i++)
+        double det = GetDeterminant();
+        SquareMatrix algebraicadd = GetAlgebraicAddition();
+        SquareMatrix trans = GetTranspose();
+
+        SquareMatrix result = new SquareMatrix(_data.GetLength(0));
+
+
+        for (int i = 0; i < result.Size; i++)
         {
-            for (int j = 0; j < result.GetLength(1); j++)
+            for (int j = 0; j < result.Size; j++)
             {
                 result[i, j] = trans[i, j] / det;
             }
@@ -107,17 +112,28 @@ public class SquareMatrix
         return result;
     }
 
-    double[,] GetAlgebraicAddition()
+    private SquareMatrix GetAlgebraicAddition()
     {
-        double[,] result = new double[matrix.GetLength(0), matrix.GetLength(1)];
-        for (int i = 0; i < matrix.GetLength(0); i++)
+        for (int i = 0; i < _data.GetLength(0); i++)
         {
-            for (int j = 0; j < matrix.GetLength(1); j++)
+            for (int j = 0; j < _data.GetLength(1); j++)
             {
-                double[,] without = GetMatrixWithout(matrix, i, j);
-                result[i, j] = ((i + j) % 2 is 0 ? 1 : -1) * GetDeterminant(without);
+                SquareMatrix without = GetMatrixWithout(i, j);
+                this[i, j] = ((i + j) % 2 is 0 ? 1 : -1) * GetDeterminant();
             }
         }
-        return result;
+        return this;
+    }
+    SquareMatrix GetTranspose()
+    {
+        SquareMatrix transpose = new SquareMatrix((double[,])_data.Clone());
+        for (int i = 0; i < Size; i++)
+        {
+            for (int j = 0; j < Size; j++)
+            {
+                _data[i,j] = transpose[i, j];
+            }
+        }
+        return this;
     }
 }
