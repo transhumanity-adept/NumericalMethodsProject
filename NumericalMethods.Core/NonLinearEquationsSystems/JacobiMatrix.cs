80f6d0a4ae70676148630aaed5880863116649ac
- <d.b_official@mail.ru>
2022-04-15 17:37:26 +0300

    temporary created snu

---
+++ NumericalMethods.Core/NonLinearEquationsSystems/JacobiMatrix.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumericalMethods.Core.NonLinearEquationsSystems
+{
+    public class JacobiMatrix : SquareMatrix
+    {
+        public JacobiMatrix() : base(size)
+        {
+        }
+    }
+}
