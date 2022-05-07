8dd7ff4c20414d7f72a2472be2465d54e16f658d
Данила Савенков <savenkov-lokin2013@yandex.ru>
2022-05-07 11:43:24 +0300

    Понятно, что ничего не понятно

---
+++ NumericalMethods.Core/NumericalMethods.Core.Differentiation/DifferentiationFunctions/NewtonPolynomial/NewtonPolynomialDifferentiationFunction.cs
@@ -0,0 +1,41 @@
+﻿using NumericalMethods.Core.Differentiations.Interfaces;
+
+
+namespace NumericalMethods.Core.Differentiations.DifferentiationFunctions.NewtonPolynomial
+{
+    internal class NewtonPolynomialDifferentiationFunction : DifferentiationFunctionBase, IDifferentiationFunction
+    {
+        private readonly int dimension;
+        public NewtonPolynomialDifferentiationFunction(IEnumerable<IDifferentiationNode> differentiationNodes, double step,int dimension) : base(differentiationNodes, step)
+        {
+            this.dimension = dimension;
+        }
+        private Func<double,double?> BuildNewtonPolynomial(int dimension)
+        {
+            return new Func<double, double?>((double x) =>
+            {
+                double? result = 0;
+                for (int i = 0; i < dimension; i++)
+                {
+                    result += x <= centerNode.X ? (1 - i + 1) / CalculateFactorial(i)//В данном случае q = 1,мейби расписать
+                    : (-1 - i + 1) / CalculateFactorial(i) * GetFiniteDifference(x, i);
+                }
+                return result;
+            });
+        }
+        private int CalculateFactorial(int value)
+        {
+            if (value <= 0) return 0;
+            int factorialValue = 1;
+            for (int i = 1; i <= value; i++)
+            {
+                factorialValue *= i;
+            }
+            return factorialValue;
+        }
+        public double? Calculate(double argument, int derivative_degree)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NumericalMethods.Core/NumericalMethods.Core.Differentiation/DifferentiationFunctions/Quadratic/QuadraticDifferentationFunction.cs
@@ -0,0 +1,45 @@
+﻿using NumericalMethods.Core.Differentiations.Interfaces;
+
+namespace NumericalMethods.Core.Differentiations.DifferentiationFunctions.Quadratic
+{
+    internal class QuadraticDifferentationFunction : DifferentiationFunctionBase,IDifferentiationFunction
+    {
+        public QuadraticDifferentationFunction(IEnumerable<IDifferentiationNode> differentiationNodes,double step):base(differentiationNodes,step)
+        {}
+        public double? Calculate(double argument, int derivative_degree)
+        {
+            return CalculateRecursive(argument, 1, derivative_degree);
+        }
+        public double? CalculateRecursive(double argument,int depth,int derivative_degree)
+        {   
+            double? right_y,deltY1, deltaLeft_y, deltaRight_y, deltY2;
+            deltY1 = GetFiniteDifference(argument,1);
+            deltY2 = GetFiniteDifference(argument, 2);
+            //if (interpolationFunction.Calculate(argument + step) == null)
+            //{
+            //    deltY1 = interpolationFunction.Calculate(argument) - interpolationFunction.Calculate(argument - step);
+            //    deltaLeft_y = interpolationFunction.Calculate(argument - step) - interpolationFunction.Calculate(argument - 2 * step);
+            //    deltaRight_y = deltY1;
+            //    deltY2 = deltaRight_y - deltaLeft_y;
+            //}
+            //else
+            //{
+
+            //}
+
+            var new_deph = depth + 1;
+            if (depth != derivative_degree)
+            {
+                deltY1 = GetFiniteDifference(argument, 1);
+                deltY2 = GetFiniteDifference(argument, 2);
+                right_y = CalculateRecursive(argument + step, new_deph, derivative_degree);
+                deltY1 = right_y - argument;
+                deltaLeft_y = deltY1;
+                deltaRight_y = CalculateRecursive(right_y.Value + step, new_deph, derivative_degree) - right_y;
+                deltY2 = deltaRight_y - deltaLeft_y;
+            }
+            return argument <= centerNode.X ? (deltY1 - 0.5 * deltY2) / step : (deltY1 + 0.5 * deltY2) / step;
+
+        }
+    }
+}
