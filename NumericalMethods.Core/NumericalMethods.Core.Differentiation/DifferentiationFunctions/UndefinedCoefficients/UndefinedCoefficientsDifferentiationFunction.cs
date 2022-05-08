f1fe73cfd513a087b85952fe3cacad26f5af4c07
Семён Поздняков <santaspolusa@gmail.com>
2022-05-08 20:28:09 +0300

    init undefined two

--- NumericalMethods.Core/NumericalMethods.Core.Differentiation/DifferentiationFunctions/UndefinedCoefficients/UndefinedCoefficientsDifferentiationFunction.cs
+++ NumericalMethods.Core/NumericalMethods.Core.Differentiation/DifferentiationFunctions/UndefinedCoefficients/UndefinedCoefficientsDifferentiationFunction.cs
@@ -11,10 +11,12 @@ namespace NumericalMethods.Core.Differentiation.DifferentiationFunctions.Undefin
     public class UndefinedCoefficientsDifferentiationFunction : DifferentiationFunctionBase, IDifferentiationFunction
     {
         private readonly int _matrix_size;
+        private readonly double[] _h;
         public UndefinedCoefficientsDifferentiationFunction(IEnumerable<IDifferentiationNode> differentiationNodes, double step, int derrivative_degree) 
             : base(differentiationNodes, step, derrivative_degree)
         {
             _matrix_size = _nodes.Count();
+            _h = differentiationNodes.Select(node => node.X - _first_node.X).ToArray();
         }
 
         public double? Calculate(double argument)
@@ -26,7 +28,24 @@ namespace NumericalMethods.Core.Differentiation.DifferentiationFunctions.Undefin
 
         private VectorColumn CreateVectorB()
         {
-            return new VectorColumn(Enumerable.Range(0, _matrix_size).ToArray());
+            return new VectorColumn(Enumerable.Range(0, _matrix_size).Select(value => (double)value).ToArray());
+        }
+
+        private SquareMatrix CreateMatrix()
+        {
+            double[,] matrixBody = new double[_matrix_size, _matrix_size];
+            for (int j = 0; j < _matrix_size; j++)
+            {
+                matrixBody[0, j] = 1;
+            }
+            for (int i = 1; i < _matrix_size; i++)
+            {
+                for (int j = 0; j < _matrix_size; j++)
+                {
+                    matrixBody[i, j] = _h[i] * Math.Pow(j, i);
+                }
+            }
+            return new SquareMatrix(matrixBody);
         }
     }
 }
